4a9610bca953bbaed360a541239294402ab59a65
Oleksandr Knyaz <lichknyaz@gmail.com>
2026-08-17 11:23:23 +0200

    Grant Telegram function database access

---
+++ supabase/migrations/202608170005_service_role_telegram_permissions.sql
@@ -0,0 +1,3 @@
+grant select on table public.automation_rules to service_role;
+grant select, insert, update on table public.automation_events to service_role;
+grant select on table public.leads to service_role;
